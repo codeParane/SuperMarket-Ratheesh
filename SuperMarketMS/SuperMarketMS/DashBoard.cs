ea779fc4c4d36af7cda42d99a4e2e43daa4c7bd0
codeParane <codeparane@gmail.com>
2018-09-08 15:01:17 +0530

    dashboard edit

--- SuperMarketMS/SuperMarketMS/DashBoard.cs
+++ SuperMarketMS/SuperMarketMS/DashBoard.cs
@@ -19,6 +19,36 @@ namespace SuperMarketMS
 
         private void DashBoard_Load(object sender, EventArgs e)
         {
+            if (LoginForm.loggedUser != null)
+            {
+                dbLoggedUser.Text = "Cashier : " + LoginForm.loggedUser.ToUpper();
+
+            }
+
+            dbTimer.Enabled = true;
+            dbTimer.Interval = 1000;
+            dbTimer.Tick += new EventHandler(this.tmrDateTime_Tick);
+        }
+        private void tmrDateTime_Tick(object sender, EventArgs e)
+        {
+            string dtTime = DateTime.Now.ToString("hh:mm:ss");
+            string dtDate = DateTime.Now.ToString("yyyy/mm/dd");
+            dbTimeNDate.Text = dtDate + " - " + dtTime;
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            //while
+            viewDashboard l = new viewDashboard();
+            l.TopLevel = false;
+            dbFrmContainer.Controls.Add(l);
+            l.Show();
+            //dbFrmContainer.md
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
---
+++ SuperMarketMS/SuperMarketMS/viewDashboard.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SuperMarketMS
+{
+    partial class viewDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(viewDashboard));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bunifuCircleProgressbar1 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.bunifuCircleProgressbar2 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.bunifuCircleProgressbar3 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.bunifuCircleProgressbar4 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.bunifuCircleProgressbar5 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.86344F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.13656F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 124F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 126F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 61F));
+            this.tableLayoutPanel1.Controls.Add(this.bunifuCircleProgressbar1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bunifuCircleProgressbar2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bunifuCircleProgressbar3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bunifuCircleProgressbar4, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bunifuCircleProgressbar5, 4, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.08911F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.91089F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 123F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(690, 400);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // bunifuCircleProgressbar1
+            // 
+            this.bunifuCircleProgressbar1.animated = false;
+            this.bunifuCircleProgressbar1.animationIterval = 5;
+            this.bunifuCircleProgressbar1.animationSpeed = 300;
+            this.bunifuCircleProgressbar1.BackColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar1.BackgroundImage")));
+            this.bunifuCircleProgressbar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCircleProgressbar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar1.LabelVisible = true;
+            this.bunifuCircleProgressbar1.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar1.LineThickness = 5;
+            this.bunifuCircleProgressbar1.Location = new System.Drawing.Point(10, 205);
+            this.bunifuCircleProgressbar1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar1.MaxValue = 100;
+            this.bunifuCircleProgressbar1.Name = "bunifuCircleProgressbar1";
+            this.bunifuCircleProgressbar1.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCircleProgressbar1.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar1.Size = new System.Drawing.Size(105, 105);
+            this.bunifuCircleProgressbar1.TabIndex = 0;
+            this.bunifuCircleProgressbar1.Value = 0;
+            // 
+            // bunifuCircleProgressbar2
+            // 
+            this.bunifuCircleProgressbar2.animated = false;
+            this.bunifuCircleProgressbar2.animationIterval = 5;
+            this.bunifuCircleProgressbar2.animationSpeed = 300;
+            this.bunifuCircleProgressbar2.BackColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar2.BackgroundImage")));
+            this.bunifuCircleProgressbar2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCircleProgressbar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar2.LabelVisible = true;
+            this.bunifuCircleProgressbar2.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar2.LineThickness = 5;
+            this.bunifuCircleProgressbar2.Location = new System.Drawing.Point(144, 205);
+            this.bunifuCircleProgressbar2.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar2.MaxValue = 100;
+            this.bunifuCircleProgressbar2.Name = "bunifuCircleProgressbar2";
+            this.bunifuCircleProgressbar2.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCircleProgressbar2.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar2.Size = new System.Drawing.Size(105, 105);
+            this.bunifuCircleProgressbar2.TabIndex = 2;
+            this.bunifuCircleProgressbar2.Value = 0;
+            // 
+            // bunifuCircleProgressbar3
+            // 
+            this.bunifuCircleProgressbar3.animated = false;
+            this.bunifuCircleProgressbar3.animationIterval = 5;
+            this.bunifuCircleProgressbar3.animationSpeed = 300;
+            this.bunifuCircleProgressbar3.BackColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar3.BackgroundImage")));
+            this.bunifuCircleProgressbar3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCircleProgressbar3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar3.LabelVisible = true;
+            this.bunifuCircleProgressbar3.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar3.LineThickness = 5;
+            this.bunifuCircleProgressbar3.Location = new System.Drawing.Point(263, 205);
+            this.bunifuCircleProgressbar3.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar3.MaxValue = 100;
+            this.bunifuCircleProgressbar3.Name = "bunifuCircleProgressbar3";
+            this.bunifuCircleProgressbar3.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCircleProgressbar3.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar3.Size = new System.Drawing.Size(105, 105);
+            this.bunifuCircleProgressbar3.TabIndex = 2;
+            this.bunifuCircleProgressbar3.Value = 0;
+            // 
+            // bunifuCircleProgressbar4
+            // 
+            this.bunifuCircleProgressbar4.animated = false;
+            this.bunifuCircleProgressbar4.animationIterval = 5;
+            this.bunifuCircleProgressbar4.animationSpeed = 300;
+            this.bunifuCircleProgressbar4.BackColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar4.BackgroundImage")));
+            this.bunifuCircleProgressbar4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCircleProgressbar4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar4.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar4.LabelVisible = true;
+            this.bunifuCircleProgressbar4.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar4.LineThickness = 5;
+            this.bunifuCircleProgressbar4.Location = new System.Drawing.Point(388, 205);
+            this.bunifuCircleProgressbar4.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar4.MaxValue = 100;
+            this.bunifuCircleProgressbar4.Name = "bunifuCircleProgressbar4";
+            this.bunifuCircleProgressbar4.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCircleProgressbar4.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar4.Size = new System.Drawing.Size(105, 105);
+            this.bunifuCircleProgressbar4.TabIndex = 2;
+            this.bunifuCircleProgressbar4.Value = 0;
+            // 
+            // bunifuCircleProgressbar5
+            // 
+            this.bunifuCircleProgressbar5.animated = false;
+            this.bunifuCircleProgressbar5.animationIterval = 5;
+            this.bunifuCircleProgressbar5.animationSpeed = 300;
+            this.bunifuCircleProgressbar5.BackColor = System.Drawing.Color.White;
+            this.bunifuCircleProgressbar5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar5.BackgroundImage")));
+            this.bunifuCircleProgressbar5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCircleProgressbar5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar5.LabelVisible = true;
+            this.bunifuCircleProgressbar5.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar5.LineThickness = 5;
+            this.bunifuCircleProgressbar5.Location = new System.Drawing.Point(512, 205);
+            this.bunifuCircleProgressbar5.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar5.MaxValue = 100;
+            this.bunifuCircleProgressbar5.Name = "bunifuCircleProgressbar5";
+            this.bunifuCircleProgressbar5.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCircleProgressbar5.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar5.Size = new System.Drawing.Size(105, 105);
+            this.bunifuCircleProgressbar5.TabIndex = 2;
+            this.bunifuCircleProgressbar5.Value = 0;
+            // 
+            // viewDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 400);
+            this.ControlBox = false;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "viewDashboard";
+            this.Text = "viewDashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar1;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar2;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar3;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar4;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar5;
+    }
+}
